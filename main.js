86d1f7fd8e616fafa1e864888e4c29e0433cc0b4
Adilet N <94730216+adilet-nasirov@users.noreply.github.com>
2021-11-30 17:37:19 -0800

    Added fortune teller code

---
+++ main.js
@@ -0,0 +1,16 @@
+
+
+// console.log("Secret code");
+// document.write("   this code is from js");
+// debugger;
+// const myName="ADILET";
+// console.log("I am "+24 +"years old");
+
+var loc= "usa";
+var job= "Enterpeneur";
+var numOfChild= 7;
+var Z= "Jessica";
+
+function fortune(){
+    document.write("You will be "+job+" in the "+loc+ ". And married to "+Z+ ", u'll have "+numOfChild+" kids.");
+}
